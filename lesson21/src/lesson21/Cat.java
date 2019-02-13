a7f7dfad3650b3092c66f7232607c5bf2019806a
dinakrasnova <dinakrasnova@bk.ru>
2019-02-13 14:27:51 +0500

    Java2les1

---
+++ lesson21/src/lesson21/Cat.java
@@ -0,0 +1,28 @@
+package lesson21;
+
+public class Cat extends Animal implements Jumpable, Swimable {
+    private int swim_limit;
+    private float jump_limit;
+
+    public Cat(String name) {
+        this.name = name;
+        this.run_limit = 100;
+        swim_limit = 10;
+        jump_limit = 3.8f;
+    }
+
+    @Override
+    public String voice() {
+        return "мяу";
+    }
+
+    @Override
+    public boolean swim(int length) {
+        return swim_limit >= length;
+    }
+
+    @Override
+    public boolean jump(float height) {
+        return jump_limit >= height;
+    }
+}
