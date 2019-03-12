c26c22c9c05182668d8f401985e26e3a7235a045
dinakrasnova <dinakrasnova@bk.ru>
2019-03-12 08:37:25 +0500

    A11

---
+++ HelloActivity/app/src/main/java/ru/geekbrains/MainActivity.java
@@ -0,0 +1,39 @@
+package ru.geekbrains;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+public class MainActivity extends AppCompatActivity implements GreetingStrings
+{ // Наследуем класс Activity
+    // или его потомка
+// Этот метод вызывается при создании Activity
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super .onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView greeting = (TextView) findViewById(R.id.greeting); // Получить   элемент
+        BuilderGreetingPhrase builderGreetingPhrase = new
+                BuilderGreetingPhrase( this );
+        greeting.setText(builderGreetingPhrase.get()); // Заменить значение
+    }
+    @Override
+    public String getHelloer () {
+        return getResources().getString(R.string.helloer);
+    }
+    @Override
+    public String getMorning () {
+        return getResources().getString(R.string.morning);
+    }
+    @Override
+    public String getAfternoon () {
+        return getResources().getString(R.string.afternoon);
+    }
+    @Override
+    public String getEvening () {
+        return getResources().getString(R.string.evening);
+    }
+    @Override
+    public String getNight () {
+        return getResources().getString(R.string.night);
+    }
+}
